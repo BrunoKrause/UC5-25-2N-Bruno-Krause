a92ae7c6bf5d464e066b59fbcf104253ebcbcf86
BRUNO KRAUSE NOIA DA COSTA <02155268050@senacrs.edu.br>
2026-05-28 21:47:22 -0300

    Começo

---
+++ EnglishActivity/activity.js
@@ -0,0 +1,13 @@
+import rl from "readline-sync"
+
+function start() {
+
+    console.log(`
+    You are Welcome to Error Detector!
+    Let's go improve and test your English level!
+    `)
+    
+    let continuar = rl.question("Press ENTER to continue...")
+    
+
+}
